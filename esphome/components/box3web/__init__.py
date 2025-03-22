71b4eb3876f56d0ed9ec26bab4d0692eebacdf71
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-22 02:47:50 +0100

    Update __init__.py

--- esphome/components/box3web/__init__.py
+++ esphome/components/box3web/__init__.py
@@ -1,39 +1,55 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
-from esphome.core import CORE
+from esphome.components import web_server_base
+from esphome.components.web_server_base import CONF_WEB_SERVER_BASE_ID
+from esphome.const import (
+    CONF_ID
+)
+from esphome.core import coroutine_with_priority, CORE
+from .. import sd_mmc_card
 
-DEPENDENCIES = ['esp32']
-CODEOWNERS = ['@your-github-username']
+CONF_URL_PREFIX = "url_prefix"
+CONF_ROOT_PATH = "root_path"
+CONF_ENABLE_DELETION = "enable_deletion"
+CONF_ENABLE_DOWNLOAD = "enable_download"
+CONF_ENABLE_UPLOAD = "enable_upload"
 
-box3web_ns = cg.esphome_ns.namespace('box3web')
-Box3WebComponent = box3web_ns.class_('Box3WebComponent', cg.Component)
+AUTO_LOAD = ["web_server_base"]
+DEPENDENCIES = ["sd_mmc_card"]
 
-CONF_BOX3WEB = 'box3web'
-CONF_URL_PREFIX = 'url_prefix'
-CONF_ROOT_PATH = 'root_path'
-CONF_ENABLE_DELETION = 'enable_deletion'
-CONF_ENABLE_DOWNLOAD = 'enable_download'
-CONF_ENABLE_UPLOAD = 'enable_upload'
+box3web_ns = cg.esphome_ns.namespace("box3web")
+Box3Web = box3web_ns.class_("Box3Web", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Box3WebComponent),
-    cv.Optional(CONF_URL_PREFIX, default='files'): cv.string,
-    cv.Optional(CONF_ROOT_PATH, default='/'): cv.string,
-    cv.Optional(CONF_ENABLE_DELETION, default=False): cv.boolean,
-    cv.Optional(CONF_ENABLE_DOWNLOAD, default=True): cv.boolean,
-    cv.Optional(CONF_ENABLE_UPLOAD, default=False): cv.boolean,
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Box3Web),
+            cv.GenerateID(CONF_WEB_SERVER_BASE_ID): cv.use_id(
+                web_server_base.WebServerBase
+            ),
+            cv.GenerateID(sd_mmc_card.CONF_SD_MMC_CARD_ID): cv.use_id(sd_mmc_card.SdMmc),
+            cv.Optional(CONF_URL_PREFIX, default="file"): cv.string_strict,
+            cv.Optional(CONF_ROOT_PATH, default="/"): cv.string_strict,
+            cv.Optional(CONF_ENABLE_DELETION, default=False): cv.boolean,
+            cv.Optional(CONF_ENABLE_DOWNLOAD, default=False): cv.boolean,
+            cv.Optional(CONF_ENABLE_UPLOAD, default=False): cv.boolean,
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+)
 
+@coroutine_with_priority(45.0)
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    paren = await cg.get_variable(config[CONF_WEB_SERVER_BASE_ID])
     
+    var = cg.new_Pvariable(config[CONF_ID], paren)
+    await cg.register_component(var, config)
+    sdmmc = await cg.get_variable(config[sd_mmc_card.CONF_SD_MMC_CARD_ID])
+    cg.add(var.set_sd_mmc_card(sdmmc))
     cg.add(var.set_url_prefix(config[CONF_URL_PREFIX]))
     cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
-    cg.add(var.set_enable_deletion(config[CONF_ENABLE_DELETION]))
-    cg.add(var.set_enable_download(config[CONF_ENABLE_DOWNLOAD]))
-    cg.add(var.set_enable_upload(config[CONF_ENABLE_UPLOAD]))
+    cg.add(var.set_deletion_enabled(config[CONF_ENABLE_DELETION]))
+    cg.add(var.set_download_enabled(config[CONF_ENABLE_DOWNLOAD]))
+    cg.add(var.set_upload_enabled(config[CONF_ENABLE_UPLOAD]))
     
-    cg.add_build_flag('-DUSE_BOX3WEB')
+    cg.add_define("USE_SD_CARD_WEBSERVER")
     
