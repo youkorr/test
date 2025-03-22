f022fce4a2f497f039946d68e4adcc1b33f73f30
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-22 03:51:40 +0100

    Update box3web.cpp

--- esphome/components/box3web/box3web.cpp
+++ esphome/components/box3web/box3web.cpp
@@ -6,13 +6,11 @@
 namespace esphome {
 namespace box3web {
 
-static const char *TAG = "box3web";
+static const char *TAG = "";
 
 Box3Web::Box3Web(web_server_base::WebServerBase *base) : base_(base) {}
 
-void Box3Web::setup() {
-  this->base_->add_handler(this);
-}
+void Box3Web::setup() { this->base_->add_handler(this); }
 
 void Box3Web::dump_config() {
   ESP_LOGCONFIG(TAG, "Box3Web File Server:");
@@ -20,8 +18,8 @@ void Box3Web::dump_config() {
   ESP_LOGCONFIG(TAG, "  Url Prefix: %s", this->url_prefix_.c_str());
   ESP_LOGCONFIG(TAG, "  Root Path: %s", this->root_path_.c_str());
   ESP_LOGCONFIG(TAG, "  Deletion Enabled: %s", TRUEFALSE(this->deletion_enabled_));
-  ESP_LOGCONFIG(TAG, "  Download Enabled: %s", TRUEFALSE(this->download_enabled_));
-  ESP_LOGCONFIG(TAG, "  Upload Enabled: %s", TRUEFALSE(this->upload_enabled_));
+  ESP_LOGCONFIG(TAG, "  Download Enabled : %s", TRUEFALSE(this->download_enabled_));
+  ESP_LOGCONFIG(TAG, "  Upload Enabled : %s", TRUEFALSE(this->upload_enabled_));
 }
 
 bool Box3Web::canHandle(AsyncWebServerRequest *request) {
@@ -38,22 +36,6 @@ void Box3Web::handleRequest(AsyncWebServerRequest *request) {
       this->handle_delete(request);
       return;
     }
-    // Gérer les requêtes POST pour les uploads
-    if (request->method() == HTTP_POST && request->hasHeader("Content-Type")) {
-      AsyncWebHeader* content_type_header = request->getHeader("Content-Type");
-      if (content_type_header && strstr(content_type_header->value().c_str(), "multipart/form-data") != nullptr) {
-        for (size_t i = 0; i < request->args(); i++) {
-          AsyncWebParameter* param = request->getParam(i);
-          if (param->isFile()) {
-            std::string file_name = param->name().c_str();
-            uint8_t* data = (uint8_t*)param->value().c_str();
-            size_t len = param->value().length();
-            this->handleUpload(request, file_name.c_str(), 0, data, len, true);
-          }
-        }
-        return;
-      }
-    }
   }
 }
 
@@ -63,40 +45,24 @@ void Box3Web::handleUpload(AsyncWebServerRequest *request, const String &filenam
     request->send(401, "application/json", "{ \"error\": \"file upload is disabled\" }");
     return;
   }
-
   std::string extracted = this->extract_path_from_url(std::string(request->url().c_str()));
   std::string path = this->build_absolute_path(extracted);
 
-  // Vérifier si le chemin est un dossier valide
   if (index == 0 && !this->sd_mmc_card_->is_directory(path)) {
     auto response = request->beginResponse(401, "application/json", "{ \"error\": \"invalid upload folder\" }");
     response->addHeader("Connection", "close");
     request->send(response);
     return;
   }
-
   std::string file_name(filename.c_str());
-  std::string full_file_path = Path::join(path, file_name);
-
   if (index == 0) {
-    ESP_LOGD(TAG, "Uploading file %s to %s", file_name.c_str(), path.c_str());
-    if (!this->sd_mmc_card_->write_file(full_file_path.c_str(), data, len)) {
-      auto response = request->beginResponse(500, "application/json", "{ \"error\": \"failed to write file\" }");
-      response->addHeader("Connection", "close");
-      request->send(response);
-      return;
-    }
-  } else {
-    if (!this->sd_mmc_card_->append_file(full_file_path.c_str(), data, len)) {
-      auto response = request->beginResponse(500, "application/json", "{ \"error\": \"failed to append to file\" }");
-      response->addHeader("Connection", "close");
-      request->send(response);
-      return;
-    }
+    ESP_LOGD(TAG, "uploading file %s to %s", file_name.c_str(), path.c_str());
+    this->sd_mmc_card_->write_file(Path::join(path, file_name).c_str(), data, len);
+    return;
   }
-
+  this->sd_mmc_card_->append_file(Path::join(path, file_name).c_str(), data, len);
   if (final) {
-    auto response = request->beginResponse(201, "text/html", "Upload success");
+    auto response = request->beginResponse(201, "text/html", "upload success");
     response->addHeader("Connection", "close");
     request->send(response);
     return;
@@ -160,8 +126,8 @@ std::string Box3Web::format_size(uint64_t size) const {
 void Box3Web::write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const {
   std::string uri = "/" + Path::join(this->url_prefix_, Path::remove_root_path(info.path, this->root_path_));
   std::string file_name = Path::file_name(info.path);
-  std::string file_type = this->get_file_type(file_name); // Déterminer le type de fichier
-  uint64_t file_size = info.size; // Taille du fichier
+  std::string file_type = get_file_type(file_name);
+  uint64_t file_size = info.size;
 
   response->print("<tr><td>");
   if (info.is_directory) {
@@ -174,9 +140,9 @@ void Box3Web::write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo con
     response->print(file_name.c_str());
   }
   response->print("</td><td>");
-  response->print(file_type.c_str()); // Afficher le type de fichier
+  response->print(file_type.c_str());
   response->print("</td><td>");
-  response->print(this->format_size(file_size).c_str()); // Afficher la taille du fichier
+  response->print(format_size(file_size).c_str());
   response->print("</td><td>");
   if (!info.is_directory) {
     if (this->download_enabled_) {
@@ -197,54 +163,36 @@ void Box3Web::write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo con
 
 void Box3Web::handle_index(AsyncWebServerRequest *request, std::string const &path) const {
   AsyncResponseStream *response = request->beginResponseStream("text/html");
-  response->print(F(
-      "<!DOCTYPE html>"
-      "<html lang=\"en\">"
-      "<head>"
-      "<meta charset=\"UTF-8\">"
-      "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\">"
-      "</head>"
-      "<body>"
-      "<h1>SD Card Content</h1>"
-      "<h2>Folder "));
+  response->print(F("<!DOCTYPE html><html lang=\"en\"><head><meta charset=UTF-8><meta "
+                    "name=viewport content=\"width=device-width, initial-scale=1,user-scalable=no\">"
+                    "</head><body>"
+                    "<h1>SD Card Content</h1><h2>Folder "));
+
   response->print(path.c_str());
   response->print(F("</h2>"));
-
-  if (this->upload_enabled_) {
-    response->print(F(
-        "<form method=\"POST\" enctype=\"multipart/form-data\">"
-        "<input type=\"file\" name=\"file\">"
-        "<input type=\"submit\" value=\"Upload\">"
-        "</form>"));
-  }
-
+  if (this->upload_enabled_)
+    response->print(F("<form method=\"POST\" enctype=\"multipart/form-data\">"
+                      "<input type=\"file\" name=\"file\"><input type=\"submit\" value=\"upload\"></form>"));
   response->print(F("<a href=\"/"));
   response->print(this->url_prefix_.c_str());
-  response->print(F("\">Home</a></br></br>"
-                    "<table id=\"files\">"
-                    "<thead><tr><th>Name<th>Type<th>Size<th>Actions</tr></thead>"
-                    "<tbody>"));
-
+  response->print(F("\">Home</a></br></br><table id=\"files\"><thead><tr><th>Name<th>Type<th>Size<th>Actions<tbody>"));
   auto entries = this->sd_mmc_card_->list_directory_file_info(path, 0);
-  for (auto const &entry : entries) {
+  for (auto const &entry : entries)
     write_row(response, entry);
-  }
 
   response->print(F("</tbody></table>"
                     "<script>"
                     "function navigate_to(path) { window.location.href = path; }"
-                    "function delete_file(path) { fetch(path, { method: 'DELETE' }); }"
+                    "function delete_file(path) {fetch(path, {method: \"DELETE\"});}"
                     "function download_file(path, filename) {"
-                    "fetch(path)"
-                    ".then(response => response.blob())"
+                    "fetch(path).then(response => response.blob())"
                     ".then(blob => {"
                     "const link = document.createElement('a');"
                     "link.href = URL.createObjectURL(blob);"
                     "link.download = filename;"
                     "link.click();"
-                    "})"
-                    ".catch(console.error);"
-                    "}"
+                    "}).catch(console.error);"
+                    "} "
                     "</script>"
                     "</body></html>"));
 
@@ -257,28 +205,18 @@ void Box3Web::handle_download(AsyncWebServerRequest *request, std::string const 
     return;
   }
 
-  // Lire le fichier depuis la carte SD/MMC
   auto file = this->sd_mmc_card_->read_file(path);
   if (file.size() == 0) {
-    request->send(404, "application/json", "{ \"error\": \"file not found\" }");
+    request->send(401, "application/json", "{ \"error\": \"failed to read file\" }");
     return;
   }
-
-  // Extraire le nom du fichier pour l'en-tête Content-Disposition
-  std::string file_name = Path::file_name(path);
-
-  // Créer une réponse avec le contenu du fichier
 #ifdef USE_ESP_IDF
-  auto *response = request->beginResponse_P(200, "application/octet-stream", file.data(), file.size());
+  auto *response = request->beginResponse_P(200, "application/octet", file.data(), file.size());
 #else
-  auto *response = request->beginResponseStream("application/octet-stream", file.size());
+  auto *response = request->beginResponseStream("application/octet", file.size());
   response->write(file.data(), file.size());
 #endif
 
-  // Ajouter l'en-tête Content-Disposition pour forcer le téléchargement
-  response->addHeader("Content-Disposition", ("attachment; filename=\"" + file_name + "\"").c_str());
-
-  // Envoyer la réponse
   request->send(response);
 }
 
@@ -317,6 +255,38 @@ std::string Box3Web::build_absolute_path(std::string relative_path) const {
 
   std::string absolute = Path::join(this->root_path_, relative_path);
   return absolute;
+}
+
+std::string Path::file_name(std::string const &path) {
+  size_t pos = path.rfind(Path::separator);
+  if (pos != std::string::npos) {
+    return path.substr(pos + 1);
+  }
+  return "";
+}
+
+bool Path::is_absolute(std::string const &path) { return path.size() && path[0] == separator; }
+
+bool Path::trailing_slash(std::string const &path) { return path.size() && path[path.length() - 1] == separator; }
+
+std::string Path::join(std::string const &first, std::string const &second) {
+  std::string result = first;
+  if (!trailing_slash(first) && !is_absolute(second)) {
+    result.push_back(separator);
+  }
+  if (trailing_slash(first) && is_absolute(second)) {
+    result.pop_back();
+  }
+  result.append(second);
+  return result;
+}
+
+std::string Path::remove_root_path(std::string path, std::string const &root) {
+  if (!str_startswith(path, root))
+    return path;
+  if (path.size() == root.size() || path.size() < 2)
+    return "/";
+  return path.erase(0, root.size());
 }
 
 }  // namespace box3web
