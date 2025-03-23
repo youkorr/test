419c155a6893ca9994d8b8bed8c4aac412c24587
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 17:49:23 +0100

    Update webdav.h

--- esphome/components/sd_webdav/webdav.h
+++ esphome/components/sd_webdav/webdav.h
@@ -1,31 +1,40 @@
 #pragma once
-#include "esphome.h"
-#include <AsyncWebServer_ESP32_SC_W5500.h>
-#include <ESPAsyncWebDav.h>
+
+#include "esphome/core/component.h"
+#include "esphome/components/web_server_base/web_server_base.h"
 #include "../sd_mmc_card/sd_mmc_card.h"
 
 namespace esphome {
-namespace sd_webdav {
+namespace webdav {
 
-class SDWebDAV : public Component {
+class WebDAVComponent : public Component, public AsyncWebHandler {
  public:
-  void set_sd_card(sdmc::SDMmcCard *sd_card) { sd_card_ = sd_card; }
+  void set_sd_card(sdcard::SDCard *sd_card) { sd_card_ = sd_card; }
   void set_mount_point(const std::string &mount_point) { mount_point_ = mount_point; }
   void set_username(const std::string &username) { username_ = username; }
   void set_password(const std::string &password) { password_ = password; }
 
   void setup() override;
-  void loop() override;
+  void loop() override {}
+
+  bool canHandle(AsyncWebServerRequest *request) override;
+  void handleRequest(AsyncWebServerRequest *request) override;
 
  protected:
-  sdmc::SDMmcCard *sd_card_;
+  sdcard::SDCard *sd_card_{nullptr};
   std::string mount_point_;
   std::string username_;
   std::string password_;
-  AsyncWebServer *server_{nullptr};
-  AsyncWebdav *dav_{nullptr};
+
+  bool authenticate(AsyncWebServerRequest *request);
+  void handlePropfind(AsyncWebServerRequest *request);
+  void handleGet(AsyncWebServerRequest *request);
+  void handlePut(AsyncWebServerRequest *request);
+  void handleDelete(AsyncWebServerRequest *request);
+  void handleMkcol(AsyncWebServerRequest *request);
 };
 
-}  // namespace sd_webdav
+}  // namespace webdav
 }  // namespace esphome
+
 
