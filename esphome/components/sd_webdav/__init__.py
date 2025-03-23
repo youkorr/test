e88668ce3b36c67d4660b97b0fd4d7c3683a43dd
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 17:22:41 +0100

    Update __init__.py

--- esphome/components/sd_webdav/__init__.py
+++ esphome/components/sd_webdav/__init__.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import web_server_base
+from esphome.components import web_server_base, sdcard
 from esphome.const import CONF_ID, CONF_USERNAME, CONF_PASSWORD
 
-DEPENDENCIES = ['web_server_base']
+CODEOWNERS = ["@youkorr"]
+DEPENDENCIES = ['web_server_base', 'sdcard']
 AUTO_LOAD = ['web_server_base']
 
 CONF_SD_CARD_ID = 'sd_card_id'
@@ -14,17 +15,17 @@ SDWebDAVComponent = sd_webdav_ns.class_('SDWebDAVComponent', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDWebDAVComponent),
-    cv.Required(CONF_SD_CARD_ID): cv.use_id(cg.esphome_ns.SDCard),
+    cv.Required(CONF_SD_CARD_ID): cv.use_id(sdcard.SDCardComponent),
     cv.Required(CONF_MOUNT_POINT): cv.string,
     cv.Optional(CONF_USERNAME, default=''): cv.string,
     cv.Optional(CONF_PASSWORD, default=''): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
     
-    sd_card = yield cg.get_variable(config[CONF_SD_CARD_ID])
+    sd_card = await cg.get_variable(config[CONF_SD_CARD_ID])
     cg.add(var.set_sd_card(sd_card))
     
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
