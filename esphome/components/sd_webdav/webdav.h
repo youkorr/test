ab545bdb4fdcbc4bf535e507ab4f3053b2d8801b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 18:36:15 +0100

    Update webdav.h

--- esphome/components/sd_webdav/webdav.h
+++ esphome/components/sd_webdav/webdav.h
@@ -15,16 +15,10 @@ class SDWebDAVComponent : public Component {
   
   void set_sd_card(sd_mmc_card::SdMmc *sd_card) { sd_card_ = sd_card; }
   void set_mount_point(const std::string &mount_point) { mount_point_ = mount_point; }
-  void set_credentials(const std::string &username, const std::string &password) {
-    username_ = username;
-    password_ = password;
-  }
 
  protected:
   sd_mmc_card::SdMmc *sd_card_;
   std::string mount_point_;
-  std::string username_;
-  std::string password_;
   web_server_base::WebServerBase *web_server_;
   static const char *const TAG;
 };
