6ac1311c76408c6607bb2bf6ed723c094c20949c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 18:44:25 +0100

    Update webdav.h

--- esphome/components/sd_webdav/webdav.h
+++ esphome/components/sd_webdav/webdav.h
@@ -1,43 +1,27 @@
 #pragma once
-#include "webdav.h"
+
+#include "esphome.h"
 #include "esphome/components/web_server_base/web_server_base.h"
 #include "../sd_mmc_card/sd_mmc_card.h"
 
 namespace esphome {
 namespace sd_webdav {
 
-static const char *const TAG = "webdav";
+class SDWebDAVComponent : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  
+  void set_sd_card(sd_mmc_card::SdMmc *sd_card) { sd_card_ = sd_card; }
+  void set_mount_point(const std::string &mount_point) { mount_point_ = mount_point; }
 
-void SDWebDAVComponent::setup() {
-  web_server_ = new web_server_base::WebServerBase();
-  
-  // Initialize WebDAV server
-  web_server_->init();
-  
-  // Initialize SD card
-  sd_card_->setup();
-  if (!sd_card_->is_ready()) {
-    ESP_LOGE(TAG, "Failed to initialize SD card!");
-    return;
-  }
-  
-  // Set up WebDAV routes
-  web_server_->add_handler("/", HTTP_GET, [this](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "WebDAV Server Running");
-  });
-  
-  // Start server
-  web_server_->begin();
-}
-
-void SDWebDAVComponent::loop() {
-  // Handle WebDAV requests
-}
-
-void SDWebDAVComponent::dump_config() {
-  ESP_LOGCONFIG(TAG, "WebDAV Server:");
-  ESP_LOGCONFIG(TAG, "  Mount Point: %s", mount_point_.c_str());
-}
+ protected:
+  sd_mmc_card::SdMmc *sd_card_;
+  std::string mount_point_;
+  web_server_base::WebServerBase *web_server_;
+  static const char *const TAG;
+};
 
 }  // namespace sd_webdav
 }  // namespace esphome
