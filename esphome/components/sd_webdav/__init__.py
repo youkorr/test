49aeed1e456bc9b02fb2e0690099e8222bb5a3ff
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 18:35:30 +0100

    Update __init__.py

--- esphome/components/sd_webdav/__init__.py
+++ esphome/components/sd_webdav/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import web_server_base
-from esphome.const import CONF_ID, CONF_USERNAME, CONF_PASSWORD
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ['web_server_base']
 AUTO_LOAD = ['web_server_base']
@@ -16,8 +16,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDWebDAVComponent),
     cv.Required(CONF_SD_CARD_ID): cv.use_id(cg.esphome_ns.SDMMCCard),
     cv.Required(CONF_MOUNT_POINT): cv.string,
-    cv.Optional(CONF_USERNAME, default=''): cv.string,
-    cv.Optional(CONF_PASSWORD, default=''): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
@@ -27,10 +25,6 @@ def to_code(config):
     sd_card = yield cg.get_variable(config[CONF_SD_CARD_ID])
     cg.add(var.set_sd_card(sd_card))
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
-    cg.add(var.set_credentials(
-        config[CONF_USERNAME],
-        config[CONF_PASSWORD]
-    ))
 
 
 
