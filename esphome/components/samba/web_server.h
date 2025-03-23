4cab98bee67660fb590e5017e5958ad1043950b5
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 06:59:24 +0100

    Update web_server.h

--- esphome/components/samba/web_server.h
+++ esphome/components/samba/web_server.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/web_server_base/web_server_base.h"
+#include "esp_http_server.h"
+#include "esp_log.h"
+#include <string>
 
 namespace esphome {
 namespace samba_server {
@@ -9,18 +10,19 @@ namespace samba_server {
 class WebServer {
  public:
   void setup();
-  void loop();
+  void loop() {} // ESP-IDF HTTP server doesn't need a loop
 
- protected:
-  AsyncWebServer server_{80};
+ private:
+  httpd_handle_t server_ = NULL;
   
-  void handle_list_directory(AsyncWebServerRequest *request);
-  void handle_download_file(AsyncWebServerRequest *request);
-  void handle_upload_file(AsyncWebServerRequest *request);
-  void handle_delete_file(AsyncWebServerRequest *request);
-  void handle_rename_file(AsyncWebServerRequest *request);
+  static esp_err_t handle_list_directory(httpd_req_t *req);
+  static esp_err_t handle_download_file(httpd_req_t *req);
+  static esp_err_t handle_upload_file(httpd_req_t *req);
+  static esp_err_t handle_delete_file(httpd_req_t *req);
+  static esp_err_t handle_rename_file(httpd_req_t *req);
 };
 
 }  // namespace samba_server
 }  // namespace esphome
+
 
