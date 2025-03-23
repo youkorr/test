d49c9b3e156f16ab588f4b977a7b0c85f6a4890e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 06:56:38 +0100

    Update web_server.h

--- esphome/components/samba/web_server.h
+++ esphome/components/samba/web_server.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include <ESPAsyncWebServer.h>
+#include "esphome/components/web_server_base/web_server_base.h"
 
 namespace esphome {
 namespace samba_server {
