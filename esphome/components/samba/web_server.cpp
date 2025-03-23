1f914a9f263ba7726bc2a7660f5d9dc41cbb2656
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 06:20:53 +0100

    Create web_server.cpp

---
+++ esphome/components/samba/web_server.cpp
@@ -0,0 +1,46 @@
+#include "web_server.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace samba_server {
+
+static const char *TAG = "samba_web_server";
+
+void WebServer::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up Web Server...");
+  
+  server_.on("/list", HTTP_GET, std::bind(&WebServer::handle_list_directory, this, std::placeholders::_1));
+  server_.on("/download", HTTP_GET, std::bind(&WebServer::handle_download_file, this, std::placeholders::_1));
+  server_.on("/upload", HTTP_POST, std::bind(&WebServer::handle_upload_file, this, std::placeholders::_1));
+  server_.on("/delete", HTTP_POST, std::bind(&WebServer::handle_delete_file, this, std::placeholders::_1));
+  server_.on("/rename", HTTP_POST, std::bind(&WebServer::handle_rename_file, this, std::placeholders::_1));
+  
+  server_.begin();
+}
+
+void WebServer::loop() {
+  // The AsyncWebServer doesn't need a loop method
+}
+
+void WebServer::handle_list_directory(AsyncWebServerRequest *request) {
+  // TODO: Implement directory listing
+}
+
+void WebServer::handle_download_file(AsyncWebServerRequest *request) {
+  // TODO: Implement file download
+}
+
+void WebServer::handle_upload_file(AsyncWebServerRequest *request) {
+  // TODO: Implement file upload
+}
+
+void WebServer::handle_delete_file(AsyncWebServerRequest *request) {
+  // TODO: Implement file/directory deletion
+}
+
+void WebServer::handle_rename_file(AsyncWebServerRequest *request) {
+  // TODO: Implement file/directory renaming
+}
+
+}  // namespace samba_server
+}  // namespace esphome
