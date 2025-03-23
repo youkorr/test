0399bc098f49489a4354b60856f0bc9d17b0cb0b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 05:32:35 +0100

    Update __init__.py

--- esphome/components/samba/__init__.py
+++ esphome/components/samba/__init__.py
@@ -1,36 +1,50 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import web_server_base, sd_mmc_card
 from esphome.const import CONF_ID
+from esphome.components import web_server_base
 
 DEPENDENCIES = ['web_server_base']
-AUTO_LOAD = ['sd_mmc_card']
+AUTO_LOAD = ['web_server_base']
 
 samba_ns = cg.esphome_ns.namespace('samba')
 SambaComponent = samba_ns.class_('SambaComponent', cg.Component)
 
-CONF_WEB_SERVER_BASE_ID = 'web_server_base_id'
-CONF_SD_MMC_CARD_ID = 'sd_mmc_card'
-CONF_SHARE_NAME = 'share_name'
+CONF_WORKGROUP = 'workgroup'
 CONF_ROOT_PATH = 'root_path'
+CONF_SD_MMC_CARD = 'sd_mmc_card'
+CONF_LOCAL_MASTER = 'local_master'
+CONF_USERNAME = 'username'
+CONF_PASSWORD = 'password'
+CONF_ENABLED_SHARES = 'enabled_shares'
+CONF_ALLOW_HOSTS = 'allow_hosts'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SambaComponent),
-    cv.GenerateID(CONF_WEB_SERVER_BASE_ID): cv.use_id(web_server_base.WebServerBase),
-    cv.Required(CONF_SD_MMC_CARD_ID): cv.use_id(sd_mmc_card.SdMmc),
-    cv.Optional(CONF_SHARE_NAME, default='ESP32'): cv.string,
-    cv.Optional(CONF_ROOT_PATH, default='/sdcard'): cv.string,
+    cv.Required(CONF_WORKGROUP): cv.string,
+    cv.Required(CONF_ROOT_PATH): cv.string,
+    cv.Required(CONF_SD_MMC_CARD): cv.use_id(cg.Component),
+    cv.Optional(CONF_LOCAL_MASTER, default=True): cv.boolean,
+    cv.Required(CONF_USERNAME): cv.string,
+    cv.Required(CONF_PASSWORD): cv.string,
+    cv.Optional(CONF_ENABLED_SHARES, default=[]): cv.ensure_list(cv.string),
+    cv.Optional(CONF_ALLOW_HOSTS, default=[]): cv.ensure_list(cv.string),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    
-    web_server = await cg.get_variable(config[CONF_WEB_SERVER_BASE_ID])
-    cg.add(var.set_web_server(web_server))
-    
-    sd_card = await cg.get_variable(config[CONF_SD_MMC_CARD_ID])
-    cg.add(var.set_sd_mmc_card(sd_card))
-    
-    cg.add(var.set_share_name(config[CONF_SHARE_NAME]))
+
+    cg.add(var.set_workgroup(config[CONF_WORKGROUP]))
     cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
+    sd_mmc = await cg.get_variable(config[CONF_SD_MMC_CARD])
+    cg.add(var.set_sd_mmc_card(sd_mmc))
+    cg.add(var.set_local_master(config[CONF_LOCAL_MASTER]))
+    cg.add(var.set_username(config[CONF_USERNAME]))
+    cg.add(var.set_password(config[CONF_PASSWORD]))
+
+    for share in config[CONF_ENABLED_SHARES]:
+        cg.add(var.add_enabled_share(share))
+
+    for host in config[CONF_ALLOW_HOSTS]:
+        cg.add(var.add_allowed_host(host))
+
