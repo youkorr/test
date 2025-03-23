936c35296478bafeea50b7bd4c66aa81595b241d
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 18:01:00 +0100

    Update webdav.cpp

--- esphome/components/sd_webdav/webdav.cpp
+++ esphome/components/sd_webdav/webdav.cpp
@@ -1,7 +1,6 @@
 #include "webdav.h"
 #include "esphome/core/log.h"
 #include "esphome/core/application.h"
-#include <SD.h>
 
 namespace esphome {
 namespace webdav {
@@ -49,24 +48,19 @@ bool WebDAVComponent::authenticate(AsyncWebServerRequest *request) {
   if (this->username_.empty() && this->password_.empty()) {
     return true;
   }
-  if (request->authenticate(this->username_.c_str(), this->password_.c_str())) {
-    return true;
-  }
-  return false;
+  return request->authenticate(this->username_.c_str(), this->password_.c_str());
 }
 
 void WebDAVComponent::handlePropfind(AsyncWebServerRequest *request) {
-  // Implement PROPFIND method
-  // This should return XML with file/directory properties
   request->send(207, "application/xml", "<d:multistatus xmlns:d=\"DAV:\"></d:multistatus>");
 }
 
 void WebDAVComponent::handleGet(AsyncWebServerRequest *request) {
   String path = request->url().substring(this->mount_point_.length());
-  if (SD.exists(path)) {
-    File file = SD.open(path);
+  if (this->sd_card_->fs()->exists(path)) {
+    File file = this->sd_card_->fs()->open(path);
     if (file) {
-      request->send(SD, path, "application/octet-stream");
+      request->send(*this->sd_card_->fs(), path, "application/octet-stream");
       file.close();
     } else {
       request->send(404);
@@ -80,7 +74,7 @@ void WebDAVComponent::handlePut(AsyncWebServerRequest *request) {
   String path = request->url().substring(this->mount_point_.length());
   if (request->hasParam("body", true)) {
     AsyncWebParameter* p = request->getParam("body", true);
-    File file = SD.open(path, FILE_WRITE);
+    File file = this->sd_card_->fs()->open(path, FILE_WRITE);
     if (file) {
       file.print(p->value());
       file.close();
@@ -95,7 +89,7 @@ void WebDAVComponent::handlePut(AsyncWebServerRequest *request) {
 
 void WebDAVComponent::handleDelete(AsyncWebServerRequest *request) {
   String path = request->url().substring(this->mount_point_.length());
-  if (SD.remove(path)) {
+  if (this->sd_card_->fs()->remove(path)) {
     request->send(204);
   } else {
     request->send(404);
@@ -104,7 +98,7 @@ void WebDAVComponent::handleDelete(AsyncWebServerRequest *request) {
 
 void WebDAVComponent::handleMkcol(AsyncWebServerRequest *request) {
   String path = request->url().substring(this->mount_point_.length());
-  if (SD.mkdir(path)) {
+  if (this->sd_card_->fs()->mkdir(path)) {
     request->send(201);
   } else {
     request->send(500);
@@ -113,4 +107,5 @@ void WebDAVComponent::handleMkcol(AsyncWebServerRequest *request) {
 
 }  // namespace webdav
 }  // namespace esphome
+
 
