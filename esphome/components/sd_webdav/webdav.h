a7c8774f5c4d8b7dc12d5c9926cf07fcafe05d1e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 18:18:49 +0100

    Update webdav.h

--- esphome/components/sd_webdav/webdav.h
+++ esphome/components/sd_webdav/webdav.h
@@ -2,7 +2,7 @@
 
 #include "esphome.h"
 #include "esphome/components/web_server_base/web_server_base.h"
-#include "../sd_mmc_card/sd_mmc_card.h"
+#include "esphome/components/sdmmc/sdmmc.h"
 
 namespace esphome {
 namespace sd_webdav {
@@ -13,7 +13,7 @@ class SDWebDAVComponent : public Component {
   void loop() override;
   void dump_config() override;
   
-  void set_sd_card(sd_mmc_card::SDMMCCard *sd_card) { sd_card_ = sd_card; }
+  void set_sd_card(sdmmc::SDMMCComponent *sd_card) { sd_card_ = sd_card; }
   void set_mount_point(const std::string &mount_point) { mount_point_ = mount_point; }
   void set_credentials(const std::string &username, const std::string &password) {
     username_ = username;
@@ -21,11 +21,12 @@ class SDWebDAVComponent : public Component {
   }
 
  protected:
-  sd_mmc_card::SDMMCCard *sd_card_;
+  sdmmc::SDMMCComponent *sd_card_;
   std::string mount_point_;
   std::string username_;
   std::string password_;
   web_server_base::WebServerBase *web_server_;
+  static const char *const TAG;
 };
 
 }  // namespace sd_webdav
