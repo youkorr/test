fc78719a6c10cc1a05353b79ca4b3b8dac0165cf
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-23 06:58:55 +0100

    Update web_server.cpp

--- esphome/components/samba/web_server.cpp
+++ esphome/components/samba/web_server.cpp
@@ -1,46 +1,89 @@
 #include "web_server.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
 namespace samba_server {
 
-static const char *TAG = "samba_web_server";
+static const char *TAG = "web_server";
 
 void WebServer::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up Web Server...");
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   
-  server_.on("/list", HTTP_GET, std::bind(&WebServer::handle_list_directory, this, std::placeholders::_1));
-  server_.on("/download", HTTP_GET, std::bind(&WebServer::handle_download_file, this, std::placeholders::_1));
-  server_.on("/upload", HTTP_POST, std::bind(&WebServer::handle_upload_file, this, std::placeholders::_1));
-  server_.on("/delete", HTTP_POST, std::bind(&WebServer::handle_delete_file, this, std::placeholders::_1));
-  server_.on("/rename", HTTP_POST, std::bind(&WebServer::handle_rename_file, this, std::placeholders::_1));
-  
-  server_.begin();
+  ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+  if (httpd_start(&server_, &config) == ESP_OK) {
+    ESP_LOGI(TAG, "Registering URI handlers");
+    
+    httpd_uri_t list_uri = {
+        .uri       = "/list",
+        .method    = HTTP_GET,
+        .handler   = handle_list_directory,
+        .user_ctx  = this
+    };
+    httpd_register_uri_handler(server_, &list_uri);
+
+    httpd_uri_t download_uri = {
+        .uri       = "/download",
+        .method    = HTTP_GET,
+        .handler   = handle_download_file,
+        .user_ctx  = this
+    };
+    httpd_register_uri_handler(server_, &download_uri);
+
+    httpd_uri_t upload_uri = {
+        .uri       = "/upload",
+        .method    = HTTP_POST,
+        .handler   = handle_upload_file,
+        .user_ctx  = this
+    };
+    httpd_register_uri_handler(server_, &upload_uri);
+
+    httpd_uri_t delete_uri = {
+        .uri       = "/delete",
+        .method    = HTTP_POST,
+        .handler   = handle_delete_file,
+        .user_ctx  = this
+    };
+    httpd_register_uri_handler(server_, &delete_uri);
+
+    httpd_uri_t rename_uri = {
+        .uri       = "/rename",
+        .method    = HTTP_POST,
+        .handler   = handle_rename_file,
+        .user_ctx  = this
+    };
+    httpd_register_uri_handler(server_, &rename_uri);
+  }
 }
 
-void WebServer::loop() {
-  // The AsyncWebServer doesn't need a loop method
+esp_err_t WebServer::handle_list_directory(httpd_req_t *req) {
+  // Implement directory listing logic here
+  httpd_resp_send(req, "Directory listing not implemented", HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
 }
 
-void WebServer::handle_list_directory(AsyncWebServerRequest *request) {
-  // TODO: Implement directory listing
+esp_err_t WebServer::handle_download_file(httpd_req_t *req) {
+  // Implement file download logic here
+  httpd_resp_send(req, "File download not implemented", HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
 }
 
-void WebServer::handle_download_file(AsyncWebServerRequest *request) {
-  // TODO: Implement file download
+esp_err_t WebServer::handle_upload_file(httpd_req_t *req) {
+  // Implement file upload logic here
+  httpd_resp_send(req, "File upload not implemented", HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
 }
 
-void WebServer::handle_upload_file(AsyncWebServerRequest *request) {
-  // TODO: Implement file upload
+esp_err_t WebServer::handle_delete_file(httpd_req_t *req) {
+  // Implement file deletion logic here
+  httpd_resp_send(req, "File deletion not implemented", HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
 }
 
-void WebServer::handle_delete_file(AsyncWebServerRequest *request) {
-  // TODO: Implement file/directory deletion
-}
-
-void WebServer::handle_rename_file(AsyncWebServerRequest *request) {
-  // TODO: Implement file/directory renaming
+esp_err_t WebServer::handle_rename_file(httpd_req_t *req) {
+  // Implement file renaming logic here
+  httpd_resp_send(req, "File renaming not implemented", HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
 }
 
 }  // namespace samba_server
 }  // namespace esphome
+
