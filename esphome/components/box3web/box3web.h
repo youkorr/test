873c4ff4efdb16f0ffa3d6cfd796e0df938b880e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-24 23:10:34 +0100

    Update box3web.h

--- esphome/components/box3web/box3web.h
+++ esphome/components/box3web/box3web.h
@@ -1,67 +1,68 @@
+// box3web.h
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/web_server_base/web_server_base.h"
-#include "../sd_mmc_card/sd_mmc_card.h"
+#include "esphome/components/sd_mmc_card/sd_mmc_card.h"
+
+#include <string>
+#include <vector>
+#include <AsyncWebServer.h>
 
 namespace esphome {
 namespace box3web {
 
-class box3web : public Component, public AsyncWebHandler {
- public:
-  Box3Web(web_server_base::WebServerBase *);
-  void setup() override;
-  void dump_config() override;
-  bool canHandle(AsyncWebServerRequest *request) override;
-  void handleRequest(AsyncWebServerRequest *request) override;
-  void handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len,
-                    bool final) override;
-  bool isRequestHandlerTrivial() override { return false; }
-
-  void set_url_prefix(std::string const &);
-  void set_root_path(std::string const &);
-  void set_sd_mmc_card(sd_mmc_card::SdMmc *);
-  void set_deletion_enabled(bool);
-  void set_download_enabled(bool);
-  void set_upload_enabled(bool);
-
- protected:
-  web_server_base::WebServerBase *base_;
-  sd_mmc_card::SdMmc *sd_mmc_card_;
-
-  std::string url_prefix_;
-  std::string root_path_;
-  bool deletion_enabled_;
-  bool download_enabled_;
-  bool upload_enabled_;
-
-  std::string build_prefix() const;
-  std::string extract_path_from_url(std::string const &) const;
-  std::string build_absolute_path(std::string) const;
-  void write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const;
-  void handle_index(AsyncWebServerRequest *, std::string const &) const;
-  void handle_get(AsyncWebServerRequest *) const;
-  void handle_delete(AsyncWebServerRequest *);
-  void handle_download(AsyncWebServerRequest *, std::string const &) const;
-};
-
-struct Path {
+class Path {
+public:
   static constexpr char separator = '/';
 
-  /* Return the name of the file */
-  static std::string file_name(std::string const &);
-
-  /* Is the path an absolute path? */
-  static bool is_absolute(std::string const &);
-
-  /* Does the path have a trailing slash? */
-  static bool trailing_slash(std::string const &);
-
-  /* Join two path */
-  static std::string join(std::string const &, std::string const &);
-
+  static std::string file_name(std::string const &path);
+  static bool is_absolute(std::string const &path);
+  static bool trailing_slash(std::string const &path);
+  static std::string join(std::string const &first, std::string const &second);
   static std::string remove_root_path(std::string path, std::string const &root);
 };
 
-}  // namespace box3web
-}  // namespace esphome
+class Box3Web : public Component {
+public:
+  explicit Box3Web(web_server_base::WebServerBase *base);
+
+  void setup() override;
+  void dump_config() override;
+
+  bool canHandle(AsyncWebServerRequest *request);
+  void handleRequest(AsyncWebServerRequest *request);
+  void handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data,
+                    size_t len, bool final);
+
+  void set_url_prefix(std::string const &prefix);
+  void set_root_path(std::string const &path);
+  void set_sd_mmc_card(sd_mmc_card::SdMmc *card);
+  void set_deletion_enabled(bool allow);
+  void set_download_enabled(bool allow);
+  void set_upload_enabled(bool allow);
+
+private:
+  web_server_base::WebServerBase *base_;
+  sd_mmc_card::SdMmc *sd_mmc_card_{nullptr};
+  std::string url_prefix_{"/sdcard"};
+  std::string root_path_{"/sdcard"};
+  bool deletion_enabled_{false};
+  bool download_enabled_{true};
+  bool upload_enabled_{false};
+
+  void handle_get(AsyncWebServerRequest *request) const;
+  void handle_download(AsyncWebServerRequest *request, std::string const &path) const;
+  void handle_index(AsyncWebServerRequest *request, std::string const &path) const;
+  void handle_delete(AsyncWebServerRequest *request);
+
+  void write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const;
+
+  std::string build_prefix() const;
+  std::string extract_path_from_url(std::string const &url) const;
+  std::string build_absolute_path(std::string relative_path) const;
+};
+
+} // namespace box3web
+} // namespace esphome
 
