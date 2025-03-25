918fab773546fcbb46412cc6c6985eddbde2a1f8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 06:10:44 +0100

    Update webdavbox3.h

--- esphome/components/webdavbox3/webdavbox3.h
+++ esphome/components/webdavbox3/webdavbox3.h
@@ -6,6 +6,7 @@
 #include "driver/sdmmc_defs.h"
 #include "../sd_mmc_card/sd_mmc_card.h"
 #include "esp_vfs_fat.h"
+#include "tcpip_adapter.h"
 
 namespace esphome {
 namespace webdavbox3 {
@@ -15,28 +16,23 @@ public:
     void setup() override;
     void loop() override;
 
-    // Permet de définir le chemin racine
     void set_root_path(const std::string& path) { root_path_ = path; }
-
-    // Permet de définir le préfixe d'URL
     void set_url_prefix(const std::string& prefix) { url_prefix_ = prefix; }
+    void set_port(uint16_t port) { port_ = port; }
 
 private:
-    // Membres privés
     httpd_handle_t server_ = NULL;
     std::string root_path_ = "/sdcard/";
-    std::string url_prefix_ = "/webdav";  // Préfixe par défaut
+    std::string url_prefix_ = "/webdav";
+    uint16_t port_ = 8080;  // Port par défaut
 
-    // Méthodes statiques de gestion des requêtes HTTP
     static esp_err_t handle_root(httpd_req_t *req);
     static esp_err_t handle_webdav_list(httpd_req_t *req);
     static esp_err_t handle_webdav_get(httpd_req_t *req);
     static esp_err_t handle_webdav_put(httpd_req_t *req);
     static esp_err_t handle_webdav_delete(httpd_req_t *req);
 
-    // Méthode privée pour configurer le serveur HTTP
     void configure_http_server();
 };
-
 } // namespace webdavbox3
 } // namespace esphome
