733b52fddfb0718ea2cc1f60192891a324d9440b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 05:03:24 +0100

    Update webdavbox3.h

--- esphome/components/webdavbox3/webdavbox3.h
+++ esphome/components/webdavbox3/webdavbox3.h
@@ -18,10 +18,14 @@ public:
     // Permet de définir le chemin racine
     void set_root_path(const std::string& path) { root_path_ = path; }
 
+    // Permet de définir le préfixe d'URL
+    void set_url_prefix(const std::string& prefix) { url_prefix_ = prefix; }
+
 private:
     // Membres privés
     httpd_handle_t server_ = NULL;
     std::string root_path_ = "/sdcard/";
+    std::string url_prefix_ = "/webdav";  // Préfixe par défaut
 
     // Méthodes statiques de gestion des requêtes HTTP
     static esp_err_t handle_root(httpd_req_t *req);
