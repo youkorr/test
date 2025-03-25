624633a0d5a2d02d34c0ccc77c81a95e1be4bbc1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 04:08:13 +0100

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-CODEOWNERS = ["@your_username"]
+CODEOWNERS = ["@youkorr"]
 DEPENDENCIES = ['web_server', 'sd_mmc_card']
 MULTI_CONF = True
 
@@ -10,17 +10,16 @@ WebDAVBox3 = webdavbox3_ns.class_('WebDAVBox3', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.Optional('root_path', default='/sdcard/'): cv.string,
-})
+}).extend(cv.COMPONENT_SCHEMA)  # Important: utilise le schéma de base du composant
 
 async def to_code(config):
-    # Create a new component variable
-    var = cg.new_Pvariable(config['id'])
+    # Crée un ID automatique si non spécifié
+    var = cg.new_Pvariable(config[CONF_ID])  # Utilise l'ID généré automatiquement
     
-    # Register as a component
     await cg.register_component(var, config)
     
-    # Set root path if specified
     if 'root_path' in config:
         cg.add(var.set_root_path(config['root_path']))
     
     return var
+
