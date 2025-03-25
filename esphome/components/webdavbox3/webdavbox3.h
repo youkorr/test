f28383ceffaca51b7d152396403d7b1d822f137b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 03:50:29 +0100

    Update webdavbox3.h

--- esphome/components/webdavbox3/webdavbox3.h
+++ esphome/components/webdavbox3/webdavbox3.h
@@ -1,35 +1,36 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/web_server/web_server.h"
+#include "esphome/core/helpers.h"
+#include "esphome/core/application.h"
 #include "../sd_mmc_card/sd_mmc_card.h"
+#include <esp_http_server.h>
 
 namespace esphome {
 namespace webdavbox3 {
 
-class WebDAVBox3 : public web_server::WebServer {
+class WebDAVBox3 : public Component {
 public:
     void setup() override;
     void loop() override;
 
-protected:
-    void handle_webdav_request_();
-    
-    // Méthodes de gestion des requêtes WebDAV
-    void handle_get_file_(const String& path);
-    void handle_put_file_(const String& path);
-    void handle_delete_file_(const String& path);
-    void list_directory_(const String& path);
+    void set_root_path(const std::string& path) {
+        root_path_ = path.c_str();
+    }
 
 private:
+    // Serveur web asynchrone
+    AsyncWebServer server_{80};
+    
     // Chemin racine pour les opérations WebDAV
-    String root_path_{"/"}; 
+    String root_path_{"/sdcard/"};
 
-public:
-    // Setter pour le chemin racine
-    void set_root_path(const std::string& path) {
-        root_path_ = String(path.c_str());
-    }
+    // Méthodes internes de gestion WebDAV
+    void handle_webdav_get_(AsyncWebServerRequest *request);
+    void handle_webdav_put_(AsyncWebServerRequest *request);
+    void handle_webdav_delete_(AsyncWebServerRequest *request);
+    void handle_webdav_list_(AsyncWebServerRequest *request);
+    void handle_root_(AsyncWebServerRequest *request);
 };
 
 } // namespace webdavbox3
