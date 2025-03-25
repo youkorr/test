30ffc4cdf0dfa0d220d3b083966b7f87bba3825c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 06:11:14 +0100

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_PORT
 
 CODEOWNERS = ["@your_username"]
 DEPENDENCIES = ['web_server', 'sd_mmc_card']
@@ -10,20 +10,18 @@ webdavbox3_ns = cg.esphome_ns.namespace('webdavbox3')
 WebDAVBox3 = webdavbox3_ns.class_('WebDAVBox3')
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.Required('id'): cv.declare_id(WebDAVBox3),
+    cv.Required(CONF_ID): cv.declare_id(WebDAVBox3),
     cv.Optional('root_path', default='/sdcard/'): cv.string,
-    cv.Optional('url_prefix', default='/webdav'): cv.string,  # Nouveau paramètre
+    cv.Optional('url_prefix', default='/webdav'): cv.string,
+    cv.Optional(CONF_PORT, default=8080): cv.port,
 })
 
 async def to_code(config):
-    # Créer une nouvelle instance avec l'ID fourni
     var = cg.new_Pvariable(config[CONF_ID])
     
-    if 'root_path' in config:
-        cg.add(var.set_root_path(config['root_path']))
-    
-    if 'url_prefix' in config:
-        cg.add(var.set_url_prefix(config['url_prefix']))  # Ajouter le préfixe
+    cg.add(var.set_root_path(config['root_path']))
+    cg.add(var.set_url_prefix(config['url_prefix']))
+    cg.add(var.set_port(config[CONF_PORT]))
     
     return var
 
