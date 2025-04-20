17c0a4258fbd4f223df2aebcfbdee5bed967494e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-20 04:29:12 +0200

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_PORT
 
+# Définir des constantes pour les options username et password
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 
@@ -12,41 +13,43 @@ MULTI_CONF = True
 webdavbox3_ns = cg.esphome_ns.namespace('webdavbox3')
 WebDAVBox3 = webdavbox3_ns.class_('WebDAVBox3', cg.Component)
 
-# Validation des options username et password
+# Fonction pour valider les options username et password
 def validate_auth(config):
     if (CONF_USERNAME in config) != (CONF_PASSWORD in config):
         raise cv.Invalid("Both username and password must be specified together")
     return config
 
-# Schéma de configuration
+# Schéma de configuration, incluant username et password
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
         cv.Required(CONF_ID): cv.declare_id(WebDAVBox3),
         cv.Optional('root_path', default='/sdcard/'): cv.string,
         cv.Optional('url_prefix', default='/webdav'): cv.string,
         cv.Optional(CONF_PORT, default=8080): cv.port,
+        # Assurer que username et password sont bien ajoutés ici
         cv.Optional(CONF_USERNAME): cv.string,
         cv.Optional(CONF_PASSWORD): cv.string,
-    }).extend(cv.COMPONENT_SCHEMA),
-    validate_auth
+    }).extend(cv.COMPONENT_SCHEMA),  # Extends pour inclure les composants standards
+    validate_auth  # Validation des options
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Ajout des valeurs de configuration
+    # Configurer les chemins et autres options
     cg.add(var.set_root_path(config['root_path']))
     cg.add(var.set_url_prefix(config['url_prefix']))
     cg.add(var.set_port(config[CONF_PORT]))
 
-    # Ajout des paramètres username et password
+    # Ajouter username et password si présents
     if CONF_USERNAME in config:
         cg.add(var.set_username(config[CONF_USERNAME]))
     if CONF_PASSWORD in config:
         cg.add(var.set_password(config[CONF_PASSWORD]))
 
     return var
+
 
 
 
