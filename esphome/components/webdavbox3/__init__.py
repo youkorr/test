94b752b85e8059c99d478ba01d19c7f637257974
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-20 05:00:10 +0200

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -2,12 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_USERNAME, CONF_PASSWORD, CONF_PORT
 
-CODEOWNERS = ["@youkorr"]
+CODEOWNERS = ["@your_username"]
 DEPENDENCIES = ["web_server_base", "sd_mmc_card"]
-MULTI_CONF = False
+MULTI_CONF = False  # Si tu prévois un seul composant, sinon mets True si c'est une liste
 
-webdavbox3_ns = cg.esphome_ns.namespace("webdavbox3")
-WebDAVBox3 = webdavbox3_ns.class_("WebDAVBox3", cg.Component)
+webdavbox_ns = cg.esphome_ns.namespace("webdavbox3")
+WebDAVBox3 = webdavbox_ns.class_("WebDAVBox3", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.declare_id(WebDAVBox3),
@@ -21,14 +21,18 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
     cg.add(var.set_root_path(config["root_path"]))
     cg.add(var.set_url_prefix(config["url_prefix"]))
     cg.add(var.set_port(config[CONF_PORT]))
-
+    
     if CONF_USERNAME in config:
         cg.add(var.set_username(config[CONF_USERNAME]))
     if CONF_PASSWORD in config:
         cg.add(var.set_password(config[CONF_PASSWORD]))
+    
+    return var
+
 
 
 
