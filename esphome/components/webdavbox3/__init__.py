48133346d21dca6f113bd8ecab98af57cad571f8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-20 04:12:03 +0200

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -1,13 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_PORT, CONF_USERNAME, CONF_PASSWORD
-
 CODEOWNERS = ["@your_username"]
 DEPENDENCIES = ['web_server', 'sd_mmc']
 MULTI_CONF = True
 
 webdavbox3_ns = cg.esphome_ns.namespace('webdavbox3')
 WebDAVBox3 = webdavbox3_ns.class_('WebDAVBox3', cg.Component)
+
+def validate_auth(config):
+    if (CONF_USERNAME in config) != (CONF_PASSWORD in config):
+        raise cv.Invalid("Both username and password must be specified together")
+    return config
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
@@ -18,16 +22,6 @@ CONFIG_SCHEMA = cv.All(
         cv.Optional(CONF_USERNAME): cv.string,
         cv.Optional(CONF_PASSWORD): cv.string,
     }).extend(cv.COMPONENT_SCHEMA),
-    cv.has_at_least_one_key(CONF_USERNAME, CONF_PASSWORD),
-)
-
-def validate_auth(config):
-    if (CONF_USERNAME in config) != (CONF_PASSWORD in config):
-        raise cv.Invalid("Both username and password must be specified together")
-    return config
-
-FINAL_VALIDATE_SCHEMA = cv.All(
-    CONFIG_SCHEMA,
     validate_auth
 )
 
