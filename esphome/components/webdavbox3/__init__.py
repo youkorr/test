e48557e58c8369a1ad348e929ff60ddd128b8f1a
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-20 04:55:54 +0200

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -1,40 +1,35 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import (
-    CONF_ID,
-    CONF_USERNAME,
-    CONF_PASSWORD,
-)
-from esphome.components import web_server_base
+from esphome.const import CONF_ID, CONF_USERNAME, CONF_PASSWORD, CONF_PORT
 
-DEPENDENCIES = ['web_server_base', 'sd_mmc_card']
+CODEOWNERS = ["@youkorr"]
+DEPENDENCIES = ["web_server_base", "sd_mmc_card"]
 MULTI_CONF = False
 
-webdavbox_ns = cg.esphome_ns.namespace('webdavbox3')
-WebDAVBox3 = webdavbox_ns.class_('WebDAVBox3', cg.Component)
+webdavbox3_ns = cg.esphome_ns.namespace("webdavbox3")
+WebDAVBox3 = webdavbox3_ns.class_("WebDAVBox3", cg.Component)
 
-# Schéma de configuration
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(WebDAVBox3),
-    cv.GenerateID(web_server_base.CONF_WEB_SERVER_BASE_ID): cv.use_id(web_server_base.WebServerBase),
-    cv.Optional(CONF_USERNAME, default=""): cv.string,  # Ajout de username
-    cv.Optional(CONF_PASSWORD, default=""): cv.string,  # Ajout de password
+    cv.Required(CONF_ID): cv.declare_id(WebDAVBox3),
+    cv.Optional("root_path", default="/sdcard/"): cv.string,
+    cv.Optional("url_prefix", default="/webdav"): cv.string,
+    cv.Optional(CONF_PORT, default=8080): cv.port,
+    cv.Optional(CONF_USERNAME, default=""): cv.string,
+    cv.Optional(CONF_PASSWORD, default=""): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    cg.add(var.set_root_path(config["root_path"]))
+    cg.add(var.set_url_prefix(config["url_prefix"]))
+    cg.add(var.set_port(config[CONF_PORT]))
 
-    base = await cg.get_variable(config[web_server_base.CONF_WEB_SERVER_BASE_ID])
-    cg.add(var.set_base(base))
-
-    # Si username est présent dans la configuration, l'ajouter à l'objet
     if CONF_USERNAME in config:
-        cg.add(var.set_username(config[CONF_USERNAME]))  # Assignation de username
-    
-    # Si password est présent dans la configuration, l'ajouter à l'objet
+        cg.add(var.set_username(config[CONF_USERNAME]))
     if CONF_PASSWORD in config:
-        cg.add(var.set_password(config[CONF_PASSWORD]))  # Assignation de password
+        cg.add(var.set_password(config[CONF_PASSWORD]))
+
 
 
 
