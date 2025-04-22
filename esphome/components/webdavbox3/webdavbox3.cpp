1518447189bea08f1660348e1e69143136992518
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-22 06:07:23 +0200

    Update webdavbox3.cpp

--- esphome/components/webdavbox3/webdavbox3.cpp
+++ esphome/components/webdavbox3/webdavbox3.cpp
@@ -157,26 +157,11 @@ void WebDAVBox3::start_server() {
 }
 
 void WebDAVBox3::setup() {
-  sdmmc_host_t host = SDMMC_HOST_DEFAULT();
-  sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-
-  esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-      .format_if_mount_failed = false,
-      .max_files = 5,
-      .allocation_unit_size = 16 * 1024};
-
-  sdmmc_card_t *card;
-  esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
-
-  if (ret != ESP_OK) {
-    ESP_LOGE("webdav", "Failed to mount SD card: %s", esp_err_to_name(ret));
-    return;
-  }
-
-  ESP_LOGI("webdav", "SD card mounted successfully");
+  // Ne pas monter la carte SD ici, elle est déjà montée par le composant sd_mmc_card.
+  ESP_LOGI("webdav", "Utilisation du montage SD existant à /sdcard");
 
   configure_http_server();
-  start_server();  // Démarre le serveur WebDAV après la configuration
+  start_server();  // Démarre le serveur WebDAV
 }
 
 void WebDAVBox3::loop() {
