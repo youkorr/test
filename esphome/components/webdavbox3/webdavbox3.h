fc3bb6ac427140a7bdcae28c5546ba02e2832a59
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-23 06:48:26 +0200

    Update webdavbox3.h

--- esphome/components/webdavbox3/webdavbox3.h
+++ esphome/components/webdavbox3/webdavbox3.h
@@ -61,6 +61,7 @@ class WebDAVBox3 : public Component {
   static esp_err_t handle_webdav_copy(httpd_req_t *req);
   static esp_err_t handle_webdav_lock(httpd_req_t *req);
   static esp_err_t handle_webdav_unlock(httpd_req_t *req);
+  static esp_err_t handle_webdav_proppatch(httpd_req_t *req);
   
   // Helper methods
   static std::string get_file_path(httpd_req_t *req, const std::string &root_path);
