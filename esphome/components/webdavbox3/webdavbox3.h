9171f7f2c3b1cbf45b4a01171d2b81e76389e0d2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-24 06:42:28 +0200

    Update webdavbox3.h

--- esphome/components/webdavbox3/webdavbox3.h
+++ esphome/components/webdavbox3/webdavbox3.h
@@ -13,9 +13,6 @@
 namespace esphome {
 namespace webdavbox3 {
 
-// Declare url_decode as a standalone function
-std::string url_decode(const std::string &src);
-
 class WebDAVBox3 : public Component {
  public:
   void setup() override;
@@ -48,28 +45,23 @@ class WebDAVBox3 : public Component {
   
   // WebDAV path conversion
   std::string uri_to_filepath(const char* uri);
-
-    // Nouveau gestionnaire générique
-  static esp_err_t handle_webdav_generic(httpd_req_t *req);
   
   // WebDAV handler methods
-  static esp_err_t handle_root(httpd_req_t *req);
-  static esp_err_t handle_webdav_list(httpd_req_t *req);
+  static esp_err_t handle_root_method(httpd_req_t *req);
+  static esp_err_t handle_webdav_generic(httpd_req_t *req);
   static esp_err_t handle_webdav_options(httpd_req_t *req);
-  static esp_err_t handle_webdav_propfind(httpd_req_t *req);
   static esp_err_t handle_webdav_get(httpd_req_t *req);
   static esp_err_t handle_webdav_put(httpd_req_t *req);
   static esp_err_t handle_webdav_delete(httpd_req_t *req);
+  static esp_err_t handle_webdav_propfind(httpd_req_t *req);
   static esp_err_t handle_webdav_mkcol(httpd_req_t *req);
   static esp_err_t handle_webdav_move(httpd_req_t *req);
   static esp_err_t handle_webdav_copy(httpd_req_t *req);
   static esp_err_t handle_webdav_lock(httpd_req_t *req);
   static esp_err_t handle_webdav_unlock(httpd_req_t *req);
   static esp_err_t handle_webdav_proppatch(httpd_req_t *req);
-  static esp_err_t handle_webdav_request(httpd_req_t *req);
   
   // Helper methods
-  static std::string get_file_path(httpd_req_t *req, const std::string &root_path);
   static bool is_dir(const std::string &path);
   static std::vector<std::string> list_dir(const std::string &path);
   static std::string generate_prop_xml(const std::string &href, bool is_directory, time_t modified, size_t size);
@@ -77,7 +69,6 @@ class WebDAVBox3 : public Component {
 
 }  // namespace webdavbox3
 }  // namespace esphome
-
 
 
 
