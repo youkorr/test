24ab186f30f275a14f66c7979664de568c6ff661
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-24 21:59:27 +0200

    Update webdavbox3.h

--- esphome/components/webdavbox3/webdavbox3.h
+++ esphome/components/webdavbox3/webdavbox3.h
@@ -4,9 +4,6 @@
 #include "esphome/core/helpers.h"
 #include <string>
 #include <vector>
-#include "driver/sdmmc_host.h"
-#include "driver/sdmmc_defs.h"
-#include "../sd_mmc_card/sd_mmc_card.h"
 #include "esp_vfs_fat.h"
 #include "esp_netif.h"
 
