b1283fb96533f0f4c77323b6fa58a1129e80c692
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-24 22:42:01 +0200

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_USERNAME, CONF_PASSWORD, CONF_PORT
+from .. import sd_mmc_card
 
 CODEOWNERS = ["@youkorr"]
-DEPENDENCIES = ['network']
+DEPENDENCIES = ["sd_mmc_card"]
 
 MULTI_CONF = False  # Si tu prévois un seul composant, sinon mets True si c'est une liste
 
@@ -13,8 +14,9 @@ WebDAVBox3 = webdavbox_ns.class_("WebDAVBox3", cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.declare_id(WebDAVBox3),
     
+    cv.GenerateID(sd_mmc_card.CONF_SD_MMC_CARD_ID): cv.use_id(sd_mmc_card.SdMmc),
     cv.Optional("url_prefix", default="/"): cv.string,
-    cv.Optional("root_path", default="/sdcard/"): cv.string,
+    cv.Optional(CONF_ROOT_PATH, default="/"): cv.string_strict,
     cv.Optional(CONF_PORT, default=8081): cv.port,
     cv.Optional(CONF_USERNAME, default=""): cv.string,
     cv.Optional(CONF_PASSWORD, default=""): cv.string,
@@ -24,7 +26,8 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    
+    sdmmc = await cg.get_variable(config[sd_mmc_card.CONF_SD_MMC_CARD_ID])
+    cg.add(var.set_sd_mmc_card(sdmmc))
     cg.add(var.set_url_prefix(config["url_prefix"]))
     cg.add(var.set_port(config[CONF_PORT]))
     cg.add(var.set_root_path(config["root_path"]))
