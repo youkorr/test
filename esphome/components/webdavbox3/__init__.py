3bd87c5d2d93af5dc606ef688f7a929ce9eafd11
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-25 01:48:11 +0200

    Update __init__.py

--- esphome/components/webdavbox3/__init__.py
+++ esphome/components/webdavbox3/__init__.py
@@ -12,7 +12,7 @@ WebDAVBox3 = webdavbox_ns.class_("WebDAVBox3", cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.declare_id(WebDAVBox3),
     cv.Optional("root_path", default="/sdcard/"): cv.string,
-    cv.Optional(CONF_PORT, default=8081): cv.port,
+    cv.Optional(CONF_PORT, default=80): cv.port,
     cv.Optional(CONF_USERNAME, default=""): cv.string,
     cv.Optional(CONF_PASSWORD, default=""): cv.string,
     cv.Optional("auth_enabled", default=False): cv.boolean,
