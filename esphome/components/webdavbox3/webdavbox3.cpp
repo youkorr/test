47c8e77a06e2b12f4b024484fe19f5686e4bca0e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-26 01:32:19 +0200

    Update webdavbox3.cpp

--- esphome/components/webdavbox3/webdavbox3.cpp
+++ esphome/components/webdavbox3/webdavbox3.cpp
@@ -90,7 +90,7 @@ void WebDAVBox3::configure_http_server() {
   httpd_register_uri_handler(server_, &post_uri);
   // Gestionnaire pour la racine
   httpd_uri_t root_uri = {
-    .uri       = "/",
+    .uri = "/*",
     .method = HTTP_GET,
     .handler = handle_root,
     .user_ctx = this
